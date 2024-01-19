5578ba9d3c329e2aff01b0709b7d2916ca5ca58d
induchennupati <induchennupati7@gmail.com>
2024-01-19 19:35:53 +0530

    03-01-2024

---
+++ Str_concat.py
@@ -0,0 +1,6 @@
+str1="Hello"
+str2="World"
+print ("String 1:",str1)
+print ("String 2:",str2)
+str3=str1+str2
+print("String 3:",str3)
---
+++ strings.py
@@ -0,0 +1,14 @@
+var = "Welcome To TutorialsPoint"
+print (type(var))
+#---------------
+var = 'Welcome to "Python Tutorial" from TutorialsPoint'
+print ("var:", var)
+
+var = "Welcome to 'Python Tutorial' from TutorialsPoint"
+print ("var:", var)
+#-------------------------
+var = '''Welcome to TutorialsPoint'''
+print ("var:", var)
+
+var = """Welcome to TutorialsPoint"""
+print ("var:", var)
