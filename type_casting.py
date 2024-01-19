1743c197208efaa9ad2e5cd4d089815981d131bc
induchennupati <induchennupati7@gmail.com>
2024-01-19 18:50:37 +0530

    27-12-2023

---
+++ type_casting.py
@@ -0,0 +1,26 @@
+#int() function
+int(10.20)-->10
+int(10+20j)--->TypeError
+int(True)--->1
+int(False)---->0
+int("10")---->10
+int("10.5")----ValueError
+#----------------------------
+#float():
+float(10)---->10.0
+float(10+5j)---->TypeError
+float(Type)---->1.0
+float("10")--->10.0
+float("10.5")--->10.5
+float("ten")---->
+float("0B1111")--->
+----------------------------
+complex()
+----------------------------
+other types to complex type
+complex(x)
+complex(x,y)
+--------------
+bool()
+bool(0)---->True
+bool(10)---->False
