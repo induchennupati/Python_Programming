159a00328e25fef5ee7fcc5d685da788d8715785
induchennupati <induchennupati7@gmail.com>
2024-01-20 13:26:52 +0530

    Create adding.py

---
+++ Dictionary/adding.py
@@ -0,0 +1,4 @@
+marks = {"Savita":67, "Imtiaz":88, "Laxman":91, "David":49}
+print ("marks dictionary before update: ", marks)
+marks['Laxman'] = 95
+print ("marks dictionary after update: ", marks)
