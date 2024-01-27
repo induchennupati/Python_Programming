3ba9b509b1227904e64c7104fe41a39757849938
induchennupati <induchennupati7@gmail.com>
2024-01-27 16:41:38 +0530

    23-01-2024

---
+++ Tasks/List_files_task1.py
@@ -0,0 +1,46 @@
+import os
+import pandas as pd
+from collections import Counter
+
+def list_files_and_word_freq(target_directory):
+    file_paths = []
+    word_freq = Counter()
+
+    for root, _, files in os.walk(target_directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_paths.append(file_path)
+
+            if file_path.lower().endswith(('.xlsx', '.txt')):
+               
+                if file_path.lower().endswith('.xlsx'):
+                    try:
+                        df = pd.read_excel(file_path)
+                        text = ' '.join(map(str, df.values.flatten()))
+                    except pd.errors.EmptyDataError:
+                        text = ''
+                else:
+                    try:
+                        with open(file_path, 'r', encoding='utf-8') as txt_file:
+                            text = txt_file.read()
+                    except UnicodeDecodeError:
+                        print(f"Failed to decode file '{file_path}' with encoding 'utf-8'.")
+
+                words = text.split()
+                word_freq.update(words)
+
+    print("List of file paths:")
+    for path in file_paths:
+        print(path)
+
+    print("\nWord frequency for .xlsx and .txt files:")
+    for word, frequency in word_freq.items():
+        print(f"{word}: {frequency}")
+
+target_directory = input("Enter the target directory path: ")
+
+if os.path.exists(target_directory) and os.path.isdir(target_directory):
+    list_files_and_word_freq(target_directory)
+else:
+    print("Invalid directory path. Please provide a valid directory.")
+
--- Tasks/Task1.py
+++ Tasks/Task1.py
@@ -1,5 +1,4 @@
 import os
-
 path = input('Input: ')
 for dirpath, dirnames, filenames in os.walk(path):
     for x in filenames:
